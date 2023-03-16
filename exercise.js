cdb612ed10428808e5acbb1e904d4057f1018825
Timothy Brown <tabrown76@gmail.com>
2023-03-16 08:59:39 -0400

    initial commit

---
+++ exercise.js
@@ -0,0 +1,35 @@
+//1
+hypertext transfer protocol
+
+//2
+uniform resource locator
+
+//3
+domanin name system
+
+//4
+a query string sets a parameter/series of parameters that modify the URL
+
+//5
+get - gets information that is then parsed into a webpage
+post - sends information 
+
+//6
+a request is what you are asking the server to do
+
+//7
+a response indicates the results of a request
+
+//8
+headers let clients/servers transfer additional info, that may change how the site functions. ex. language, date/time, cookies, etc
+
+//9
+browser turns url into ip with dns, browser makes a request to that ip, server at that ip responds, browser makes a dom and sends requests for additional resources
+
+//part 2
+//1
+
+
+//2
+104.21.66.15
+172.67.198.173
