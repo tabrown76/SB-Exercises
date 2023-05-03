e83277d6b5c483caad4f388d936f41a98187f668
Timothy Brown <tabrown76@gmail.com>
2023-05-02 22:28:10 -0400

    added session functionality

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import surveys as s
 app = Flask(__name__)
 app.secret_key = 'your_unique_secret_key'
 
-responses = []
-
 @app.route('/')
 def root_route():
     return render_template('home.html', surveys=s.surveys)
+
+@app.route('/initialize_responses', methods=["POST"])
+def initialize_responses():
+    prefix = request.form['prefix']
+    session['responses'] = []
+    return redirect(url_for('question_pages', prefix=prefix, id=0))
 
 @app.route('/<prefix>/questions/<int:id>', methods=["GET", "POST"])
 def question_pages(prefix, id): 
@@ -17,19 +21,22 @@ def question_pages(prefix, id):
     if id >= len(survey.questions):
         return redirect('/thanks')
     
-    if id != len(responses):
-        if id < len(responses):
+    if id != len(session['responses']):
+        if id < len(session['responses']):
             flash("You've already answered this question.")
         else:
             flash("Invalid question. Redirecting...")
-        return redirect(url_for('question_pages', prefix=prefix, id=len(responses)))
+
+        return redirect(url_for('question_pages', prefix=prefix, id=len(session['responses'])))
     
     question = survey.questions[id]
 
     if request.method == "POST":
         response = request.form.get("choice")
-        responses.append(response)
-        
+        responses_copy = session['responses']
+        responses_copy.append(response)
+        session['responses'] = responses_copy
+        print(responses_copy)
         return redirect(url_for('question_pages', prefix=prefix, id=id + 1))
         
     return render_template('questions.html', prefix=prefix, survey=survey, question=question, id=id)
@@ -38,7 +45,7 @@ def question_pages(prefix, id):
 def thanks_page():
     return render_template('thanks.html')
 
-@app.before_request
-def clear_responses():
-    if request.endpoint == 'root_route':
-        responses.clear()
+# @app.before_request
+# def clear_responses():
+#     if request.endpoint == 'root_route':
+#         responses.clear()
